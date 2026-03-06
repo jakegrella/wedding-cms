bba1b5582e7565afc17da113082037d2822f0372
Jake Grella <jake@jakegrella.com>
2026-03-05 18:00:44 -0800

    chore: add DATABASE_AUTH_TOKEN

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -29,6 +29,7 @@ export default buildConfig({
   db: sqliteAdapter({
     client: {
       url: process.env.DATABASE_URL || '',
+      authToken: process.env.DATABASE_AUTH_TOKEN || undefined,
     },
   }),
   sharp,
